aa1e8e4c2cc92826e1a370b8ad9b8c83e6c13da3
Nandu1975 <nandumuralikrishna10@gmail.com>
2025-06-19 23:13:00 +0530

    test1_Q1

---
+++ src/TEST1/Question1.java
@@ -0,0 +1,22 @@
+package TEST1;
+
+public class Question1 {
+    public static void main(String[] args) {
+        int a=10;
+        String str="Nandu";
+        char ch='c';
+        float f=23.4f;
+        double d=1234d;
+        boolean b=true;
+
+
+        System.out.println(a);
+        System.out.println(str);
+        System.out.println(ch);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+
+
+    }
+}
